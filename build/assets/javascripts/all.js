dcce59586e2c75c418e7a655201b3d18f1fa90a4
pashutk <djlewap@gmail.com>
2015-02-19 20:20:34 +0300

    add build

---
+++ build/assets/javascripts/all.js
@@ -0,0 +1,1 @@
+(function(){var n;n=function(){return console.log("Hello World!")},n(),$("#index.photos .photo .settings .settings-title").on("click",function(n){var e;return e=$(n.target),e.toggleClass("opened")}),$(".userpanel_hamburger").on("click",function(){return $("#sidemenu").toggleClass("hidden")})}).call(this);
